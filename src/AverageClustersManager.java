f2bdf271d8b5e779a8d3594fabea8ec748aa455a
haim <haim6678@gmail.com>
2017-12-30 19:38:19 +0200

    comments. missing cluster numbers

--- src/AverageClustersManager.java
+++ src/AverageClustersManager.java
@@ -3,9 +3,8 @@ import java.util.ArrayList;
 public class AverageClustersManager extends ClusterManager {
 
 
-
 	public AverageClustersManager(int wanted, ArrayList<Point> points) {
-		super(wanted,points);
+		super(wanted, points);
 	}
 
 	/**
@@ -13,7 +12,17 @@ public class AverageClustersManager extends ClusterManager {
 	 * according to average linkage metric
 	 */
 	@Override
-	protected double  calcDistOfTowHierarchies(Cluster A, Cluster B) {
-		return -1;
+	protected double calcDistOfTowHierarchies(Cluster A, Cluster B) {
+		ArrayList<Point> ApointList = A.getPoints();
+		ArrayList<Point> BppointList = B.getPoints();
+		int numOfPoints = ApointList.size() + BppointList.size();
+		double dist = 0;
+		for (int i = 0; i < ApointList.size(); i++) {
+			for (int j = 0; j < BppointList.size(); j++) {
+				dist += super.distanceMatrix[i][j];
+			}
+		}
+
+		return dist / numOfPoints;
 	}
 }
